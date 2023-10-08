b65f2ecd2d76908001e799a21ef7915f9df9fa1b
semajame <jamesanquillano@gmail.com>
2023-10-08 12:45:57 +0800

    update

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -6,20 +6,24 @@
   </main>
 
   <section class="cart__container">
-    <div class="cart__product">
+    <div
+      class="cart__product"
+      v-for="(product, index) in cartProducts"
+      :key="index"
+    >
       <div>
         <div class="cart__product__image">
-          <img
-            src="https://assets.website-files.com/64ed01d0d959df99c03684cc/64ef1e641d50ae58a7cccc98_feey-dTLeHGu9FAw-unsplash.jpg"
-            alt=""
-          />
+          <img :src="product.imageSrc" alt="" />
         </div>
         <div class="cart__product__info">
-          <span class="cart__product__name"> Winson</span>
-          <span class="cart__price">$24.00 USD</span>
-          <button class="cart__remove__button">Remove</button>
+          <span class="cart__product__name">{{ product.name }}</span>
+          <span class="cart__price">{{ product.price }}</span>
+          <button class="cart__remove__button" @click="removeCart(index)">
+            Remove
+          </button>
         </div>
       </div>
+
       <div class="input__container">
         <input type="number" class="quantity" />
       </div>
@@ -39,7 +43,23 @@
 import NavBar from "../components/NavBar.vue";
 export default {
   data() {
-    return {};
+    return {
+      cartProducts: [
+        {
+          name: "Winson",
+          price: "$24.00 USD",
+          imageSrc:
+            "https://assets.website-files.com/64ed01d0d959df99c03684cc/64ef1e641d50ae58a7cccc98_feey-dTLeHGu9FAw-unsplash.jpg",
+        },
+      ],
+    };
+  },
+
+  methods: {
+    removeCart(index) {
+      // Remove the product at the specified index from the cart
+      this.cartProducts.splice(index, 1);
+    },
   },
 
   components: {
